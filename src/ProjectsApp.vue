81f6cd055e9697427b21ef95532778d9d4b2a82b
Chongyi Zheng <git@zcy.dev>
2023-09-15 20:17:29 -0400

    Format for mobile

--- src/ProjectsApp.vue
+++ src/ProjectsApp.vue
@@ -139,7 +139,7 @@
     <div
       v-for="project in projects[year]"
       :key="project.name"
-      class="-mx-4 px-4 mb-4 md:md-6 py-2 border rounded-lg dark:border-gray-700"
+      class="md:-mx-4 px-1.5 md:px-4 mb-4 md:md-6 py-1 md:py-3 border rounded-lg dark:border-gray-700"
     >
       <div class="mb-1 flex flex-wrap items-center align-baseline">
         <span class="mb-0.5 mr-2">
