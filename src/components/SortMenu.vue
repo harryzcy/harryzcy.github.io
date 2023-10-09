267ee0e317a6fe762f81b6e4ee340d3a6f38a3ab
Chongyi Zheng <git@zcy.dev>
2023-10-09 13:01:12 -0400

    Support enter to select on sort menu

--- src/components/SortMenu.vue
+++ src/components/SortMenu.vue
@@ -19,23 +19,26 @@
       <MenuItems
         class="absolute left-0 mt-2 w-28 cursor-pointer rounded-md border bg-white text-sm focus:outline-none dark:border-neutral-200/5 dark:bg-neutral-900"
       >
-        <MenuItem v-for="option in options" :key="option" v-slot="{ active }">
+        <MenuItem
+          v-for="option in options"
+          :key="option"
+          v-slot="{ active, close }"
+        >
           <div
             class="first:rounded-t-md last:rounded-b-md dark:bg-neutral-200/10"
             :class="{
               'bg-neutral-200/40 dark:bg-neutral-200/20 dark:text-neutral-100':
                 active
             }"
+            @click="
+              () => {
+                emit('update:modelValue', option)
+                scrollToTop()
+                close()
+              }
+            "
           >
-            <span
-              class="flex w-full items-center px-5 py-1"
-              @click="
-                () => {
-                  emit('update:modelValue', option)
-                  scrollToTop()
-                }
-              "
-            >
+            <span class="flex w-full items-center px-5 py-1">
               <span class="-ml-3 mr-1">
                 <CheckIcon
                   class="h-3 w-3"
