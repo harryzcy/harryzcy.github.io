97710650892b0da9770a7d6f7f8647261ea52404
Chongyi Zheng <git@zcy.dev>
2023-11-27 23:44:29 -0500

    Add `Deployed` to status type

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,7 +3,7 @@ type Project = {
   full_name?: string
   start_year: number
   created_at: string
-  status: 'Ongoing' | 'Completed'
+  status: 'Ongoing' | 'Completed' | 'Deployed'
   lang: string
   lang_class: string
   url?: string
