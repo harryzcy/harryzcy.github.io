cec6ae50a37ada3e8a04af7b22494ef38b969693
Chongyi Zheng <git@zcy.dev>
2023-11-27 23:28:47 -0500

    Fix error

--- script/check_project_yaml.js
+++ script/check_project_yaml.js
@@ -1,51 +1,58 @@
 // eslint-disable-next-line no-undef
 const fs = require('fs')
 
-const data = fs.readFileSync('src/projects.yaml', 'utf8')
-const lines = data.split('\n')
-const perProjectLines = []
-for (const line of lines) {
-  if (line.startsWith('- name:')) {
-    perProjectLines.push([])
-  }
-  let fieldValue = line.replace('- ', '').trim()
-  if (fieldValue.startsWith('#')) continue
-  perProjectLines[perProjectLines.length - 1].push(fieldValue)
-}
-
-const expectedOrder = [
-  'name',
-  'full_name',
-  'url',
-  'start_year',
-  'created_at',
-  'status',
-  'lang',
-  'lang_class',
-  'release_num',
-  'description'
-]
-
-let hasError = false
-for (const lines of perProjectLines) {
-  const fields = []
+const run = () => {
+  const data = fs.readFileSync('src/projects.yaml', 'utf8')
+  const lines = data.split('\n')
+  const perProjectLines = []
   for (const line of lines) {
-    const [field] = line.split(':')
-    if (field === 'description') {
-      break
+    if (line.startsWith('- name:')) {
+      perProjectLines.push([])
     }
-    fields.push(field.trim())
+    let fieldValue = line.replace('- ', '').trim()
+    if (fieldValue.startsWith('#')) continue
+    perProjectLines[perProjectLines.length - 1].push(fieldValue)
   }
-  const orderFiltered = expectedOrder.filter((field) => fields.includes(field))
-  const equal = fields.every((field, index) => field === orderFiltered[index])
-  if (!equal) {
-    console.log(`Project ${lines[0]} has incorrect field order`)
-    console.log(`Expected: ${orderFiltered.join(', ')}`)
-    console.log(`Actual: ${fields.join(', ')}`)
-    hasError = true
+
+  const expectedOrder = [
+    'name',
+    'full_name',
+    'url',
+    'start_year',
+    'created_at',
+    'status',
+    'lang',
+    'lang_class',
+    'release_num',
+    'description'
+  ]
+
+  let hasError = false
+  for (const lines of perProjectLines) {
+    const fields = []
+    for (const line of lines) {
+      const [field] = line.split(':')
+      if (field === 'description') {
+        break
+      }
+      fields.push(field.trim())
+    }
+    const orderFiltered = expectedOrder.filter((field) =>
+      fields.includes(field)
+    )
+    const equal = fields.every((field, index) => field === orderFiltered[index])
+    if (!equal) {
+      console.log(`Project ${lines[0]} has incorrect field order`)
+      console.log(`Expected: ${orderFiltered.join(', ')}`)
+      console.log(`Actual: ${fields.join(', ')}`)
+      hasError = true
+    }
+  }
+
+  if (hasError) {
+    throw new Error('Project YAML check failed')
   }
 }
 
-if (hasError) {
-  throw new Error('Project YAML check failed')
-}
+// eslint-disable-next-line no-undef
+module.exports = run
