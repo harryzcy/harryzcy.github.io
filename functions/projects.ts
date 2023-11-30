b5a1d7867716afe42a2d3e173f5fbba982e677c7
Chongyi Zheng <git@zcy.dev>
2023-11-30 02:41:06 -0500

    Log user agent

--- functions/projects.ts
+++ functions/projects.ts
@@ -1,4 +1,6 @@
 export const onRequest: PagesFunction = async (context) => {
+  const userAgent = context.request.headers.get('user-agent')
+  console.log(userAgent)
   const asset = await context.env.ASSETS.fetch('/projects')
   return new Response(asset.body, asset)
 }
