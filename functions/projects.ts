37442ece735ad288253faba68ba7504d3bacec27
Chongyi Zheng <git@zcy.dev>
2023-11-30 02:46:32 -0500

    Return raw yaml for chatgpt

--- functions/projects.ts
+++ functions/projects.ts
@@ -1,8 +1,11 @@
 export const onRequest: PagesFunction = async (context) => {
-  const userAgent = context.request.headers.get('user-agent')
-  console.log(userAgent)
-
   const url = new URL(context.request.url)
+
+  const userAgent = context.request.headers.get('user-agent')
+  if (userAgent && userAgent.includes('ChatGPT-User')) {
+    url.pathname = '/projects.yaml'
+  }
+
   const asset = await context.env.ASSETS.fetch(url)
   return new Response(asset.body, asset)
 }
