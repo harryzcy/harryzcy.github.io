34cd1bb4a74147058c69cece55f3fc5313a28671
Chongyi Zheng <git@zcy.dev>
2023-11-30 02:42:43 -0500

    Fix fetch error

--- functions/projects.ts
+++ functions/projects.ts
@@ -1,6 +1,8 @@
 export const onRequest: PagesFunction = async (context) => {
   const userAgent = context.request.headers.get('user-agent')
   console.log(userAgent)
-  const asset = await context.env.ASSETS.fetch('/projects')
+
+  const url = new URL(context.request.url)
+  const asset = await context.env.ASSETS.fetch(url)
   return new Response(asset.body, asset)
 }
