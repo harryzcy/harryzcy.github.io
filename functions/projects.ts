69e9e6e08a47a6aa20a033e66b47ad0b71fdcf8a
Chongyi Zheng <git@zcy.dev>
2023-11-30 02:48:29 -0500

    Include gpt bot as well

--- functions/projects.ts
+++ functions/projects.ts
@@ -2,7 +2,8 @@ export const onRequest: PagesFunction = async (context) => {
   const url = new URL(context.request.url)
 
   const userAgent = context.request.headers.get('user-agent')
-  if (userAgent && userAgent.includes('ChatGPT-User')) {
+  const gptUsers = ['GPTBot', 'ChatGPT-User']
+  if (userAgent && gptUsers.some((user) => userAgent.includes(user))) {
     url.pathname = '/projects.yaml'
   }
 
