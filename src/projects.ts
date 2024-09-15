965007b05825dd0dee2337f549f3c183a5350dbf
Chongyi Zheng <git@zcy.dev>
2024-09-14 21:36:12 -0400

    Fix prettier

--- src/projects.ts
+++ src/projects.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 
 import ProjectApp from './ProjectsApp.vue'
-
 ;(globalThis as any).__VUE_OPTIONS_API__ = true
 ;(globalThis as any).__VUE_PROD_DEVTOOLS__ = false
 
