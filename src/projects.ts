71790050f5f328723304cde91b2185c79e5cfbd8
Chongyi Zheng <git@zcy.dev>
2024-09-19 00:00:52 -0400

    Fix prettier

--- src/projects.ts
+++ src/projects.ts
@@ -2,7 +2,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { createApp } from 'vue'
 
-
 import ProjectApp from './ProjectsApp.vue'
 ;(globalThis as any).__VUE_OPTIONS_API__ = true
 ;(globalThis as any).__VUE_PROD_DEVTOOLS__ = false
