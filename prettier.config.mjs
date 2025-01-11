8e48ce9594ff45dbc684f9ec23a9ee962dc6539b
Chongyi Zheng <git@zcy.dev>
2025-01-11 12:47:15 -0500

    Clean up prettier config script

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,31 +1,26 @@
-const plugins = []
-// import tailwindcss from 'prettier-plugin-tailwindcss'
-// import sortImports from '@trivago/prettier-plugin-sort-imports'
-
 const config = {
   semi: false,
   singleQuote: true,
   trailingComma: 'none',
   tabWidth: 2,
   printWidth: 80,
+  plugins: []
 }
 
 try {
   await import('prettier-plugin-tailwindcss')
-  plugins.push('prettier-plugin-tailwindcss')
+  config.plugins.push('prettier-plugin-tailwindcss')
 } catch (error) {
   console.warn('Error loading prettier-plugin-tailwindcss:', error)
 }
 try {
   await import('@trivago/prettier-plugin-sort-imports')
-  plugins.push('@trivago/prettier-plugin-sort-imports')
+  config.plugins.push('@trivago/prettier-plugin-sort-imports')
   config.importOrder = ['^[./]']
   config.importOrderSeparation = true
   config.importOrderSortSpecifiers = true
 } catch (error) {
   console.warn('Error loading @trivago/prettier-plugin-sort-imports:', error)
 }
-
-config.plugins = plugins
 
 export default config
